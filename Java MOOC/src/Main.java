b275da332400902035741ba7f60983f800e4fd78
smitstroy <troyjsmits@gmail.com>
2023-12-15 12:28:08 -0500

    Beginning exercises

---
+++ Java MOOC/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int firstNum = 2;
+        int secondNum = 3;
+        System.out.println(firstNum + secondNum);
+    }
+}
